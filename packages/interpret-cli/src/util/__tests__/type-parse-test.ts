babb27edd22fcf9030cece65c386d9ff40b0a267
OF1081 <yangxiaodong@qianmi.com>
2018-04-18 08:54:37 +0800

    add: 添加 license

--- packages/interpret-cli/src/util/__tests__/type-parse-test.ts
+++ packages/interpret-cli/src/util/__tests__/type-parse-test.ts
@@ -1,12 +1,19 @@
-/**
- * @desc
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * @使用场景
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * @company qianmi.com
- * @Date    2018/3/30
- **/
-
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {jType2Ts} from "../type-parse";
 import {TypeInfoI} from "../../typings";
 
