fc9920a3d38bc21aa59edb73280422089045d14e
yangxiaodong <yangxiaodong@qianmi.com>
2018-05-28 10:42:21 +0800

    fix: bigdecimal数据可选处理..

--- packages/interpret-util/src/index.ts
+++ packages/interpret-util/src/index.ts
@@ -51,8 +51,6 @@ function minusRedundancy(itemParam: any) {
     if (itemParam.$[_key] === null || itemParam.$[_key] === undefined) {
       delete itemParam.$[_key];
       log("删除 key %s from %j ", itemParam, _key);
-    } else {
-      paramEnhance(itemParam.$[_key])
     }
   }
 }
