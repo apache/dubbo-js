2f548c29cb50d2a4ad04784eb2cd9e75bb33b41a
hufeng <hufeng@qianmi.com>
2018-07-03 14:10:48 +0800

    improve test suite

--- packages/dubbo/src/__tests__/to-test.ts
+++ packages/dubbo/src/__tests__/to-test.ts
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 import fs from 'fs';
 import {promisify} from 'util';
 import {to} from '../to';
