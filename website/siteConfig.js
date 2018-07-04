708cc421356fe0c95113d739d06e3e767c99f99a
hufeng <hufeng@qianmi.com>
2018-07-04 19:07:31 +0800

    fixed siteConfit.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,8 +24,8 @@ const siteConfig = {
   title: 'dubbo2.js' /* title for your website */,
   tagline: 'Nodejs 💕 Dubbo RPC Service',
   repoUrl: 'https://github.com/dubbo/dubbo2.js',
-  url: 'http://dubbo.io/dubbo2.js' /* your website url */,
-  baseUrl: '/' /* base url for your project */,
+  url: 'http://dubbo.io' /* your website url */,
+  baseUrl: '/dubbo2.js' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
