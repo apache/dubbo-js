9710f3fcb6c2912d3d5f898129694e3e9b2f9f88
hufeng <hufeng@qianmi.com>
2018-11-13 23:18:59 +0800

    fixed hello-egg types

--- examples/hello-egg/typings/app/controller/index.d.ts
+++ examples/hello-egg/typings/app/controller/index.d.ts
@@ -1,6 +1,7 @@
 // This file was auto created by egg-ts-helper
 // Do not modify this file!!!!!!!!!
 
+import 'egg'; // Make sure ts to import egg declaration at first
 import Home from '../../../app/controller/home';
 
 declare module 'egg' {
