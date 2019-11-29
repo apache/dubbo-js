821695911f18e09898b8d2ab6eec79e337ad0b0d
hufeng <fengmi99@gmail.com>
2019-11-30 01:06:00 +0800

    modify docker-compose zk

--- start_dubbo_service.sh
+++ start_dubbo_service.sh
@@ -25,7 +25,7 @@ mvn clean package
 # start zookeeper cluster
 docker-compose up &
 
-sleep 2
+sleep 5
 
 # start java dubbo service
 java -jar ./target/dubbo-demo-provider-2.6.3-jar-with-dependencies.jar
