e11619f70067165dc733faed466510bdd01d95f4
hufeng <fengmi99@gmail.com>
2019-11-30 11:18:14 +0800

    fixed: let java dubbo service waiting..

--- java/dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
+++ java/dubbo-demo/dubbo-demo-provider/src/main/java/com/alibaba/dubbo/demo/provider/Provider.java
@@ -10,7 +10,7 @@ public class Provider {
     public static void main(String[] args) throws Exception {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"META-INF/spring/dubbo-demo-provider.xml"});
         context.start();
-        System.in.read(); // 按任意键退出
+        // System.in.read(); // 按任意键退出
         while(true) {
             Thread.sleep(Long.MAX_VALUE);
         }
