b562a2278268d9833698487d277a4337e53ff67d
hufeng <fengmi99@gmail.com>
2021-05-27 14:47:07 +0800

    refactor: dubbo-commont test suite

--- packages/dubbo-common/src/__tests__/go-test.ts
+++ packages/dubbo-common/src/__tests__/go-test.ts
@@ -19,33 +19,34 @@ import fs from 'fs'
 import { promisify } from 'util'
 import { go } from '../go'
 
-it('test resolve', async () => {
-  const test = (): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      resolve('1')
-    })
-  }
+describe('go test suite', () => {
+  it('test promise resolve 1', async () => {
+    const test = (): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        resolve('1')
+      })
+    }
+    const { res, err } = await go(test())
+    expect(res).toEqual('1')
+    expect(err).toEqual(null)
+  })
 
-  const { res, err } = await go(test())
-  expect(res).toEqual('1')
-  expect(err).toEqual(null)
-})
+  it('test reject error', async () => {
+    const test = (): Promise<string> => {
+      return new Promise((resolve, reject) => {
+        reject(new Error('I am wrong.'))
+      })
+    }
 
-it('test reject', async () => {
-  const test = (): Promise<string> => {
-    return new Promise((resolve, reject) => {
-      reject(new Error('I am wrong.'))
-    })
-  }
+    const { res, err } = await go(test())
+    expect(res).toEqual(null)
+    expect(err.message).toEqual('I am wrong.')
+  })
 
-  const { res, err } = await go(test())
-  expect(res).toEqual(null)
-  expect(err.message).toEqual('I am wrong.')
-})
-
-it('test fs.exists async', async () => {
-  const existsPromisify = promisify(fs.exists)
-  const { res, err } = await go(existsPromisify('./to-test.ts'))
-  expect(res).toEqual(false)
-  expect(err).toEqual(null)
+  it('test fs.exists async', async () => {
+    const existsPromisify = promisify(fs.exists)
+    const { res, err } = await go(existsPromisify('./to-test.ts'))
+    expect(res).toEqual(false)
+    expect(err).toEqual(null)
+  })
 })
--- packages/dubbo-common/src/__tests__/util-test.ts
+++ packages/dubbo-common/src/__tests__/util-test.ts
@@ -29,101 +29,103 @@ import {
   Version
 } from '../util'
 
-it('msg', () => {
-  expect(msg).toBeDefined()
-  expect(msg instanceof EventEmitter).toBeTruthy()
+describe('test util method', () => {
+  it('msg pub/sub', () => {
+    expect(msg).toBeDefined()
+    expect(msg instanceof EventEmitter).toBeTruthy()
 
-  msg.on('hello', (data) => {
-    expect(data).toEqual({ name: '@apache/dubbo-js' })
-  })
-
-  msg.emit('hello', { name: '@apache/dubbo-js' })
-})
-
-it('isBoolean', () => {
-  expect(isBoolean(false)).toBeTruthy()
-  expect(isBoolean(true)).toBeTruthy()
-
-  expect(isBoolean(1)).toBeFalsy()
-  expect(isBoolean('')).toBeFalsy()
-  expect(isBoolean(undefined)).toBeFalsy()
-})
-
-it('isString', () => {
-  expect(isString('')).toBeTruthy()
-
-  expect(isString(true)).toBeFalsy()
-  expect(isString(1)).toBeFalsy()
-  expect(isString(undefined)).toBeFalsy()
-})
-
-it('isNumber', () => {
-  expect(isNumber(1)).toBeTruthy()
-  expect(isNumber(1.0)).toBeTruthy()
-
-  expect(isNumber(true)).toBeFalsy()
-  expect(isNumber('')).toBeFalsy()
-  expect(isNumber(undefined)).toBeFalsy()
-})
-
-it('isMap', () => {
-  const map = new Map()
-  expect(isMap(map)).toBeTruthy()
-
-  expect(isMap(1.0)).toBeFalsy()
-  expect(isMap(true)).toBeFalsy()
-  expect(isMap('')).toBeFalsy()
-  expect(isMap(undefined)).toBeFalsy()
-})
-
-it('isFn', () => {
-  expect(isFn(() => {})).toBeTruthy()
-
-  expect(isFn('')).toBeFalsy()
-  expect(isFn(1.0)).toBeFalsy()
-  expect(isFn(true)).toBeFalsy()
-  expect(isFn('')).toBeFalsy()
-  expect(isFn(undefined)).toBeFalsy()
-})
-
-it('checkHessianParam', () => {
-  expect(checkHessianParam({ $class: 'java.lang.Long', $: 123 })).toBeTruthy()
-  expect(checkHessianParam('')).toBeFalsy()
-  expect(checkHessianParam(1)).toBeFalsy()
-  expect(checkHessianParam(true)).toBeFalsy()
-  expect(checkHessianParam({})).toBeFalsy()
-})
-
-it('checkRetValHessian', () => {
-  expect(checkRetValHessian(undefined)).toBeTruthy()
-  expect(checkRetValHessian(true)).toBeTruthy()
-  expect(checkRetValHessian(0)).toBeTruthy()
-  expect(checkRetValHessian('')).toBeTruthy()
-  expect(checkRetValHessian(new Map())).toBeTruthy()
-
-  expect(checkRetValHessian({})).toBeFalsy()
-  expect(
-    checkRetValHessian({
-      __fields2java() {
-        return { $class: 'java.lang.String', $: 'hello' }
-      }
+    msg.on('hello', (data) => {
+      expect(data).toEqual({ name: '@apache/dubbo-js' })
     })
-  ).toBeTruthy()
-})
 
-it('trace', () => {
-  msg.on('sys:trace', (data: ITrace) => {
-    expect(data.type).toEqual('INFO')
-    expect(data.msg).toEqual('@apache/dubbo-js')
+    msg.emit('hello', { name: '@apache/dubbo-js' })
   })
-})
 
-it('version', () => {
-  expect(Version.isSupportResponseAttachment('2.0.2')).toBeTruthy()
-  expect(Version.isSupportResponseAttachment('2.0.99')).toBeTruthy()
+  it('isBoolean', () => {
+    expect(isBoolean(false)).toBeTruthy()
+    expect(isBoolean(true)).toBeTruthy()
 
-  expect(Version.isSupportResponseAttachment('2.0.0')).toBeFalsy()
-  expect(Version.isSupportResponseAttachment('2.0.199')).toBeFalsy()
+    expect(isBoolean(1)).toBeFalsy()
+    expect(isBoolean('')).toBeFalsy()
+    expect(isBoolean(undefined)).toBeFalsy()
+  })
 
-  expect(Version.getIntVersion('2.0.2')).toEqual(2000200)
+  it('isString', () => {
+    expect(isString('')).toBeTruthy()
+
+    expect(isString(true)).toBeFalsy()
+    expect(isString(1)).toBeFalsy()
+    expect(isString(undefined)).toBeFalsy()
+  })
+
+  it('isNumber', () => {
+    expect(isNumber(1)).toBeTruthy()
+    expect(isNumber(1.0)).toBeTruthy()
+
+    expect(isNumber(true)).toBeFalsy()
+    expect(isNumber('')).toBeFalsy()
+    expect(isNumber(undefined)).toBeFalsy()
+  })
+
+  it('isMap', () => {
+    const map = new Map()
+    expect(isMap(map)).toBeTruthy()
+
+    expect(isMap(1.0)).toBeFalsy()
+    expect(isMap(true)).toBeFalsy()
+    expect(isMap('')).toBeFalsy()
+    expect(isMap(undefined)).toBeFalsy()
+  })
+
+  it('isFn', () => {
+    expect(isFn(() => {})).toBeTruthy()
+
+    expect(isFn('')).toBeFalsy()
+    expect(isFn(1.0)).toBeFalsy()
+    expect(isFn(true)).toBeFalsy()
+    expect(isFn('')).toBeFalsy()
+    expect(isFn(undefined)).toBeFalsy()
+  })
+
+  it('checkHessianParam', () => {
+    expect(checkHessianParam({ $class: 'java.lang.Long', $: 123 })).toBeTruthy()
+    expect(checkHessianParam('')).toBeFalsy()
+    expect(checkHessianParam(1)).toBeFalsy()
+    expect(checkHessianParam(true)).toBeFalsy()
+    expect(checkHessianParam({})).toBeFalsy()
+  })
+
+  it('checkRetValHessian', () => {
+    expect(checkRetValHessian(undefined)).toBeTruthy()
+    expect(checkRetValHessian(true)).toBeTruthy()
+    expect(checkRetValHessian(0)).toBeTruthy()
+    expect(checkRetValHessian('')).toBeTruthy()
+    expect(checkRetValHessian(new Map())).toBeTruthy()
+
+    expect(checkRetValHessian({})).toBeFalsy()
+    expect(
+      checkRetValHessian({
+        __fields2java() {
+          return { $class: 'java.lang.String', $: 'hello' }
+        }
+      })
+    ).toBeTruthy()
+  })
+
+  it('trace', () => {
+    msg.on('sys:trace', (data: ITrace) => {
+      expect(data.type).toEqual('INFO')
+      expect(data.msg).toEqual('@apache/dubbo-js')
+    })
+  })
+
+  it('version', () => {
+    expect(Version.isSupportResponseAttachment('2.0.2')).toBeTruthy()
+    expect(Version.isSupportResponseAttachment('2.0.99')).toBeTruthy()
+
+    expect(Version.isSupportResponseAttachment('2.0.0')).toBeFalsy()
+    expect(Version.isSupportResponseAttachment('2.0.199')).toBeFalsy()
+
+    expect(Version.getIntVersion('2.0.2')).toEqual(2000200)
+  })
 })
