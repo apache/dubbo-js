14d0a098bf329d2d3129f5308edca755802487cf
jianyi-gronk <jianyi_gronk@163.com>
2023-07-13 21:15:09 +0800

    refactor: rename adapter and middleware

--- packages/dubbo-express/src/index.ts
+++ packages/dubbo-express/src/index.ts
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export { expressConnectMiddleware } from "./express-connect-middleware.js";
+export { expressConnectMiddleware } from "./express-dubbo-middleware.js";
